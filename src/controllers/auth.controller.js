637aabb55d0e4d6a61f25fa8dc1331cff163c232
Pratyush <25bcs129@iiitdwd.ac.in>
2026-06-01 23:55:44 +0530

    Add authentication middleware

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,51 @@
+const userModel = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+async function registerUser(req,res) {
+    const {username, email, password, role='user'} = req.body
+
+    const isUserAlreadyRegistered = await userModel.findOne({
+        $or: [
+            {username},
+            {email}
+        ]
+    })
+
+    if(isUserAlreadyRegistered){
+        return res.status(409).json({
+            message : "User already exists"
+        })
+    }
+
+    const hash = await bcrypt.hash(password, 10)
+
+    const user = await userModel.create({
+        username,
+        email,
+        password: hash,
+        role
+    })
+
+    const token = jwt.sign({
+        id: user._id,
+        role: user.role
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token, {
+        httpOnly : true,
+        sameSite: "lax",  
+    })
+
+    res.status(201).json({
+        message: "User registered successfully",
+        user:{
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role
+        }
+    })
+}
+
+module.exports = { registerUser }
