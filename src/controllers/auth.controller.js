b5bcd00444fc87fd4bc1273c8ad577094a4eb29c
Pratyush <25bcs129@iiitdwd.ac.in>
2026-06-02 19:16:28 +0530

    Update authentication middleware

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -48,4 +48,49 @@ async function registerUser(req,res) {
     })
 }
 
-module.exports = { registerUser }
+async function loginUser(req,res) {
+    const { username, email, password } = req.body
+
+    const user = await userModel.findOne({
+        $or: [
+            {username},
+            {email}
+        ]
+    })
+
+    if(!user){
+        return res.status(401).json({
+            message: "Invalid Credentials"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if(!isPasswordValid){
+        return res.status(401).json({
+            message: "Invalid Credentials"
+        })
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+        role: user.role
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token, {
+        httpOnly : true,
+        sameSite: "lax",  
+    })
+
+    res.status(200).json({
+        message: "User logged in successfully",
+        user:{
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role
+        }
+    })
+}
+
+module.exports = { registerUser,loginUser }
